53924b554468570c62596db6a6743aaf5e523ed3
IamJoeBiden <etzbacherelias@gmail.com>
2025-03-24 18:05:34 +0100

    ViewEntry

--- Domain/Repositories/Implementations/EntryRepository.cs
+++ Domain/Repositories/Implementations/EntryRepository.cs
@@ -1,11 +1,18 @@
-﻿using Model.Configurations;
+﻿using Domain.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Model.Configurations;
 using Model.Entities;
 
 namespace Domain.Repositories.Implementations;
 
-public class EntryRepository : ARepositoryAsync<Entry>
+public class EntryRepository : ARepositoryAsync<Entry>, IRepositoryAsync<Entry>
 {
     public EntryRepository(DiaryContext context) : base(context)
     {
+    }
+    
+    public async Task<List<Entry>> GetEntriesForUser(int userid)
+    {
+        return await Table.Where(e => e.UserId == userid).ToListAsync();
     }
 }
